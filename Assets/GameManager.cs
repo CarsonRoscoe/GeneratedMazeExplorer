ef7aef58dee858a7ad721414ec2a5ca7de70d8f4
CarsonRoscoe <carsonroscoe7@gmail.com>
2016-10-30 22:08:55 -0700

    Added Day/Night shaders activated via O/P buttons. Started Fog shader

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
 
+    private Shader m_dayShader;
+    private Shader m_nightShader;
+
     void Awake() {
         if ( Instance == null ) {
             Instance = this;
@@ -16,11 +19,42 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            SetToDay();
+        }
+        if (Input.GetKeyDown(KeyCode.P )) {
+            SetToNight();
+        }
+    }
+
+    void Start() {
+        m_dayShader = Shader.Find( "Custom/DayShader" );
+        m_nightShader = Shader.Find( "Custom/NightShader" );
+        SettingsManager.Instance.ActiveShader = m_dayShader;
+    }
+
     public void ResetRound() {
         GameObject.Find( "Player" ).GetComponent<PlayerMovement>().ResetToStart();
     }
 
     public void ToggleWalkThroughWalls() {
         SettingsManager.Instance.WalkThroughWalls = !SettingsManager.Instance.WalkThroughWalls;
+    }
+
+    public void SetToDay() {
+        SettingsManager.Instance.ActiveShader = m_dayShader;
+        ReloadWallShaders();
+    }
+
+    public void SetToNight() {
+        SettingsManager.Instance.ActiveShader = m_nightShader;
+        ReloadWallShaders();
+    }
+
+    private void ReloadWallShaders() {
+        foreach ( var wall in GameObject.FindGameObjectsWithTag( "Wall" ) ) {
+            wall.GetComponent<Renderer>().material.shader = SettingsManager.Instance.ActiveShader;
+        }
     }
 }
