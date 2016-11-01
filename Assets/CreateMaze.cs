5e583ca6db75a5988647fa6ead0c02a1c4307774
JaegarSarauer <jaegars@hotmail.com>
2016-11-01 01:26:42 -0700

    Pooling maze objects for larger maps, made UI, game reset

--- Assets/CreateMaze.cs
+++ Assets/CreateMaze.cs
@@ -10,8 +10,8 @@ public class CreateMaze : MonoBehaviour {
     private Coordinate MazeStart;
     private Coordinate MazeEnd;
     private enum MazeID { WALL, SPACE };
-    public int MazeWidth = 20;
-    public int MazeHeight = 10;
+    public int MazeWidth;
+    public int MazeHeight;
     private int mazeScale = 2;
 
     void Awake() {
@@ -25,6 +25,10 @@ public class CreateMaze : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
+        InitAll();
+    }
+
+    void InitAll() {
         InitMaze();
         CreateMazeBranches();
         DrawMaze();
@@ -78,7 +82,6 @@ public class CreateMaze : MonoBehaviour {
                 tree.Pop();
             }
         }
-
     }
 
     Coordinate FindNeighboor( Coordinate p ) {
@@ -134,12 +137,11 @@ public class CreateMaze : MonoBehaviour {
         if ( UnityEngine.Random.Range( 0, 2 ) == 0 ) {//width
             pos = (UnityEngine.Random.Range( 0, (MazeWidth - 5) / 2 ) * 2) + 3;
             ret.x = pos;
-            ret.y = UnityEngine.Random.Range( 0, 2 ) == 0 ? 0 : MazeHeight - 1;
+            ret.y = UnityEngine.Random.Range( 0, 2 ) == 0 ? 1 : MazeHeight - 2;
             return ret;
-        }
-        else {//height
+        } else {//height
             pos = (UnityEngine.Random.Range( 0, (MazeHeight - 5) / 2 ) * 2) + 3;
-            ret.x = UnityEngine.Random.Range( 0, 2 ) == 0 ? 0 : MazeWidth - 1;
+            ret.x = UnityEngine.Random.Range( 0, 2 ) == 0 ? 1 : MazeWidth - 2;
             ret.y = pos;
             return ret;
         }
@@ -162,8 +164,6 @@ public class CreateMaze : MonoBehaviour {
 
         for ( int w = 0; w < MazeWidth; w++ )
             for ( int h = 0; h < MazeHeight; h++ ) {
-                var realW = w;
-                var realH = h;
                 string resource = string.Empty;
                 if ( Maze[w, h].type == MazeID.WALL ) {
                     if ( w == 0 )
@@ -177,9 +177,24 @@ public class CreateMaze : MonoBehaviour {
                     else
                         resource = "StandardWall";
                     GameObject wall = (GameObject)Instantiate( Resources.Load( resource ) );
-                    wall.transform.position = new Vector3( realW * mazeScale, 0, realH * mazeScale );
+                    wall.transform.position = new Vector3( w * mazeScale, 0, h * mazeScale );
+                    Maze[w, h].positionObject = wall;
+                    Maze[w, h].Activate(false);
                 }
             }
+
+        GameObject endGate = (GameObject)Instantiate(Resources.Load("Gate"));
+        endGate.transform.position = new Vector3(MazeEnd.x * mazeScale, 0, MazeEnd.y * mazeScale);
+    }
+
+    public void ResetMaze() {
+        for (int w = 0; w < MazeWidth; w++)
+            for (int h = 0; h < MazeHeight; h++) {
+                Destroy(Maze[w, h].positionObject);
+            }
+        Destroy(GameObject.FindWithTag("Gate"));
+        Destroy(GameObject.FindWithTag("Floor"));
+        InitAll();
     }
 
     private void InitPlayer() {
@@ -207,25 +222,70 @@ public class CreateMaze : MonoBehaviour {
             print( "Error in CreateMaze.cs - InitPlayer() method" );
 
         startOffset.y = -.1f;
-        player.transform.position = (MazeStart.position * mazeScale) + startOffset * mazeScale;
+        player.transform.position = (MazeStart.position * mazeScale);
         player.transform.eulerAngles = startFacing;
         SettingsManager.Instance.PlayerStartPosition = player.transform.position;
         SettingsManager.Instance.PlayerStartEuler = startFacing;
+        CalculatePooling(player.transform.position);
+    }
+
+    private Coordinate WorldToCoordinate(Vector3 worldCoordinate) {
+        var w = (int)Math.Round(worldCoordinate.x / mazeScale);
+        var h = (int)Math.Round(worldCoordinate.z / mazeScale);
+        return new Coordinate(w, h);
     }
 
     public bool IsWorldCoordinateOccupied( Vector3 worldCoordinate ) {
-        var w = (int)(worldCoordinate.x / mazeScale);
-        var h = (int)(worldCoordinate.z / mazeScale);
+        Coordinate pos = WorldToCoordinate(worldCoordinate);
         bool result;
         try {
-            result = (Maze[w, h].type == MazeID.WALL);
-            print( worldCoordinate.x + " " + worldCoordinate.z + " " + result + "\n" + DateTime.Now.Ticks );
+            result = (Maze[pos.x, pos.y].type == MazeID.WALL);
         }
         catch ( Exception ) {
-            print( "Maze position" + w + " " + h + " out of index" );
             result = false;
         }
         return result;
+    }
+
+    public void CalculatePooling(Vector3 p) {
+        Coordinate pos = WorldToCoordinate(p);
+
+        for (int xx = 0; xx < MazeWidth; xx++)
+            for (int yy = 0; yy < MazeHeight; yy++)
+                Maze[xx, yy].Activate(false);
+
+        int x = 0, y = 0;
+        x = Math.Max(Math.Min(MazeWidth - 1, pos.x), 0);
+        do {
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y - 1))].Activate(true);
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y))].Activate(true);
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y + 1))].Activate(true);
+        } while (x < MazeWidth - 1 && Maze[x++, Math.Max(0, Math.Min(MazeHeight - 1, pos.y))].type == MazeID.SPACE);
+
+        x = Math.Max(Math.Min(MazeWidth - 1, pos.x), 0);
+        do {
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y - 1))].Activate(true);
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y))].Activate(true);
+            Maze[Math.Max(Math.Min(MazeWidth - 1, x), 0), Math.Max(0, Math.Min(MazeHeight - 1, pos.y + 1))].Activate(true);
+        } while (x > 0 && Maze[x--, Math.Max(0, Math.Min(MazeHeight - 1, pos.y))].type == MazeID.SPACE);
+
+        y = Math.Max(Math.Min(MazeHeight - 1, pos.y), 0);
+        do {
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x - 1)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x + 1)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+        } while (y < MazeHeight - 1 && Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x)), y++].type == MazeID.SPACE);
+
+        y = Math.Max(Math.Min(MazeHeight - 1, pos.y), 0);
+        do {
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x - 1)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+            Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x + 1)), Math.Max(0, Math.Min(MazeHeight - 1, y))].Activate(true);
+        } while (y > 0 && Maze[Math.Max(0, Math.Min(MazeWidth - 1, pos.x)), --y].type == MazeID.SPACE);
+
+        for (var xx = Math.Max(pos.x - 3, 0); xx <= Math.Min(pos.x + 3, MazeWidth - 1); xx++)
+            for (var yy = Math.Max(pos.y - 3, 0); yy <= Math.Min(pos.y + 3, MazeHeight - 1); yy++)
+                Maze[xx, yy].Activate(true);
     }
 
     private class Coordinate {
@@ -234,12 +294,15 @@ public class CreateMaze : MonoBehaviour {
         public bool visited;
         public MazeID type = MazeID.WALL;
         public Vector3 position { get { return new Vector3( x, 0, y ); } }
+        public GameObject positionObject;
+
 
         public Coordinate( int x, int y ) {
             this.x = x;
             this.y = y;
             this.visited = false;
             this.type = MazeID.WALL;
+            this.positionObject = null;
         }
 
         public Coordinate( int x, int y, MazeID id ) {
@@ -247,15 +310,23 @@ public class CreateMaze : MonoBehaviour {
             this.y = y;
             this.visited = false;
             this.type = id;
+            this.positionObject = null;
         }
 
         public bool Equals( Coordinate c ) {
             return (c.x == x && c.y == y);
         }
 
-        public string ToString() {
+        
+        public override string ToString() {
             return "x: " + x + " y:" + y;
         }
+
+        public void Activate(bool active) {
+            if (positionObject != null)
+                positionObject.SetActive(active);
+        }
+
     }
 
 }
---
+++ Assets/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public static UIManager Instance;
+
+
+    public Text EndGameText;
+
+    public Button ResetGameButton;
+    public Toggle ToggleFogButton;
+    public Toggle ToggleDayNightButton;
+    public Toggle ToggleWallClippingButton;
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        EndGameText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void EndGame() {
+        EndGameText.enabled = true;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().canMove = false;
+    }
+
+    public void ResetGame() {
+        EndGameText.enabled = false;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().canMove = true;
+        CreateMaze.Instance.ResetMaze();
+    }
+
+    public void ToggleFog(bool toggled) {
+
+    }
+
+    public void ToggleDayNight(bool toggled) {
+
+    }
+
+    public void ToggleClipping(bool toggled) {
+
+    }
+}
